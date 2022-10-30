cd90827c3e5d47ddd5508bc1f89ae83971453c03
Iker Lekuona <lekuonaiker@opendeusto.es>
2022-10-30 21:28:40 +0100

    Creación métodos lectura y escritura de ficheros.

--- ParkingProject/src/backend/Main.java
+++ ParkingProject/src/backend/Main.java
@@ -1,5 +1,7 @@
 package backend;
 
+import java.util.ArrayList;
+
 import backend.customer.OrdinaryCustomer;
 
 public class Main {
@@ -12,6 +14,14 @@ public class Main {
 		o.setVehicleType(1);
 		o.setFare(7.56);
 		System.out.println(o);
+		
+		
+		ReaderWriter rw = new ReaderWriter();
+		ArrayList<OrdinaryCustomer> prueba =  rw.ordinaryCustomerReader();
+		for(OrdinaryCustomer oc : prueba) {
+			System.out.println(oc);
+		}
+		
 	}
 
 }
