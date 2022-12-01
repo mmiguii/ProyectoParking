710b7279456fdf9af052dade3ab57d430cf15a8e
Eduardo Jorge Sanjurjo Aramburu <eduardo.sanjurjo@opendeusto.es>
2022-12-01 13:26:56 +0100

    Creacion de fichero de propiedades

---
+++ ParkingProject/src/backend/servicios/BDPropiedades.java
@@ -0,0 +1,26 @@
+package backend.servicios;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class BDPropiedades {
+
+	public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        try (FileReader reader = new FileReader("configuracion.properties")) {
+            properties.load(reader); 
+            System.out.format("Driver: %s%n", properties.getProperty("Driver"));
+            System.out.format("URL: %s%n", properties.getProperty("URL"));
+            System.out.format("Path: %s%n", properties.getProperty("Path"));
+            System.out.format("Admin: %s%n", properties.getProperty("Admin"));
+          } catch (FileNotFoundException e) {
+            System.out.println("Fichero de propiedades no encontrado");
+          } catch (IOException e) {
+            System.out.println("Error al cargar fichero de propiedades:");
+          }
+           
+    }
+}
